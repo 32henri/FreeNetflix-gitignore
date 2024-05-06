c71d52ccbac38cf3af2344050c8258eed7f0cc12
Opilane TTHK <opilane@tthk.ee>
2024-05-06 10:16:21 +0300

    FreeNetflixMaui on net ver 6 ja FreeNetflix55 on ver7, ei tea mida nende erroritega peale hakata

---
+++ FreeNetflix55/FreeNetflix55/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using FreeNetflix55.Pages;
+namespace FreeNetflix55
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            Routing.RegisterRoute(nameof(CategoriesPage), typeof(CategoriesPage));
+        }
+    }
+}
---
+++ FreeNetflix55/FreeNetflix55/MauiProgram.cs
@@ -0,0 +1,41 @@
+﻿using FreeNetflix55.Pages;
+using CommunityToolkit.Maui;
+using FreeNetflix55.Services;
+using FreeNetflix55.ViewModels;
+using Microsoft.Extensions.Logging;
+
+namespace FreeNetflix55;
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("Poppins-Regular.ttf", "PoppinsRegular");
+                fonts.AddFont("Poppins-Semibold.ttf", "PoppinsSemibold");
+            });
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        builder.Services.AddHttpClient(TmdbService.TmdbHttpClientName,
+            httpClient => httpClient.BaseAddress = new Uri("https://api.themoviedb.org"));
+
+        builder.Services.AddSingleton<TmdbService>();
+        builder.Services.AddSingleton<HomeViewModel>();
+        builder.Services.AddSingleton<MainPage>();
+
+        builder.Services.AddSingleton<CategoriesViewModel>();
+        builder.Services.AddSingleton<CategoriesPage>();
+
+        builder.Services.AddTransientWithShellRoute<DetailsPage, DetailsViewModel>(nameof(DetailsPage));
+
+        return builder.Build();
+    }
+}
+
